f1c81760ceccd6e0220d28378f3ebbb51a25d117
Glitch (lean-tortellini) <none>
2019-04-06 15:45:55 +0000

    🚞💪 Checkpoint ./parse-header.js:3676277/274

--- parse-header.js
+++ parse-header.js
@@ -1,7 +1,16 @@
 function getHeaderInfo(req, res, next) {
-  console.log("In getHeaderInfo");
+  // console.log("In getHeaderInfo");
   let header = req.headers;
-  console.log("Headers: ", header);
+  // console.log("Headers: ", header);
+  
+  let returnJSON = {
+    ipaddress: header["x-forwarded-for"].split(',')[0],
+    language: header["accept-language"],
+    software: header["user-agent"]
+  };
+
+  // console.log("Returning JSON: ", returnJSON);
+  res.json(returnJSON);
   
   next();
 }
